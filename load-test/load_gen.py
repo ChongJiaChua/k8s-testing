ef1876f771545a598551ec303928abe4e3327002
ChongJiaChua <Chong.JiaChua@stfc.ac.uk>
2026-01-07 16:49:22 +0000

    Uncomment load test code

--- load-test/load_gen.py
+++ load-test/load_gen.py
@@ -5,11 +5,11 @@ URL = "http://guestbook-ui.test-app.svc.cluster.local"
 
 print("Start") #log start
 
-# while True:
-#     try:
-#         response = urllib.request.urlopen(URL)
-#         print(f"Sent, Status Code: {response.getcode()}")
-#     except Exception as e:
-#         print(f"Error: {e}")
+while True:
+    try:
+        response = urllib.request.urlopen(URL)
+        print(f"Sent, Status Code: {response.getcode()}")
+    except Exception as e:
+        print(f"Error: {e}")
     
-#     time.sleep(0.01)
+    time.sleep(0.01)
